7060c1facd02f6b11ff0c1c72cd6d02cf1d6f5e8
AphichaiNahom(Gundam <aphichai@buzzebees.com>
2024-08-09 10:38:10 +0700

    update lib github publish

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,8 +1,8 @@
 
 object Android {
-    const val compileSdkVersion = 34
+    const val compileSdkVersion = 33
     const val minSdkVersion = 23
-    const val targetSdkVersion = 34
+    const val targetSdkVersion = 33
     const val versionCode = 1913
     const val versionName = "1.9.13"
 }
